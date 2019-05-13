e9b61cf7314c98207ec04d3230a5e3adfff1990a
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-05-13 14:47:32 -0700

    Refactor main.js to modularize code

---
+++ js/boot.js
@@ -0,0 +1,132 @@
+class BootScene extends Phaser.Scene {
+    constructor() {
+        super("BootScene");
+    }
+
+
+    preload() {
+        this.canvas1 = document.getElementsByTagName("canvas");
+        this.canvas1[0].setAttribute("id", "canvasGame");
+        this.canvasGame = document.getElementById("canvasGame");
+
+        this.load.image("earth", "./assets/images/earth.png");
+        this.load.image("logo", "./assets/images/logo.png");
+        this.load.image("bg", "./assets/images/background.png");
+        this.load.spritesheet('cat', './assets/images/nyan_cat_sprite.png', { frameWidth: 52.7, frameHeight: 22});
+    }
+
+
+    create() {
+        initializeEvents(); // Read and initialize events.json
+        initializeEndings(); // Read and initialize endings.json
+
+        this.bg = this.add.image(0, 0, "bg").setOrigin(0,0);
+        this.logo = this.add.image(this.canvasGame.width / 2, this.canvasGame.height / 2, "logo");
+        this.logo.displayWidth = this.canvasGame.width * 0.8;
+        this.logo.displayHeight = this.logo.displayWidth;
+
+        this.countSpin = 0;
+        this.canSpin = true;
+        this.logo.setInteractive();
+        this.logo.on("pointerdown", this.spinEarth, this);
+
+        this.button = this.add.text(this.canvasGame.width / 2, this.canvasGame.height / 1.35,
+            "Play", { fill: "#FFFFFF", fontSize: "3em", fontFamily: 'abel-regular'});
+        this.button.setInteractive().setOrigin(0.5, 0);
+        this.button.on("pointerdown", this.firebaseLogin, this);
+
+        this.tutorial = this.add.text(this.canvasGame.width / 2, this.canvasGame.height / 1.25,
+            "Tutorial", { fill: "#FFFFFF", fontSize: "3em", fontFamily: 'abel-regular' });
+        this.tutorial.setInteractive().setOrigin(0.5, 0);
+
+        this.options = this.add.text(this.canvasGame.width / 2, this.canvasGame.height / 1.15,
+            "Options", { fill: "#FFFFFF", fontSize: "3em", fontFamily: 'abel-regular' });
+        this.options.setInteractive().setOrigin(0.5, 0);
+
+        this.about = this.add.text(this.canvasGame.width / 2, this.canvasGame.height / 1.08,
+            "About", { fill: "#FFFFFF", fontSize: "3em", fontFamily: 'abel-regular' });
+        this.about.setInteractive().setOrigin(0.5, 0);
+
+        if (firebase.auth().currentUser) {
+            this.logout = this.add.text(this.canvasGame.width / 2, this.canvasGame.height / 1.05,
+                "Log out", {fill: "#FFFFFF", fontSize: "3em"});
+            this.logout.setInteractive().setOrigin(0.5, 0);
+            this.logout.on("pointerdown", this.firebaseLogout, this);
+        }
+    }
+
+
+    firebaseLogin() {
+        console.log("called firebaseLogin");
+        this.login = function (provider) {
+            "use strict";
+            if (!firebase.auth().currentUser) {
+                provider = new firebase.auth.GoogleAuthProvider();
+                //provider.addScope('https://www.googleapis.com/auth/plus.login');
+                provider.addScope("https://www.googleapis.com/auth/userinfo.email");
+
+                firebase.auth().signInWithRedirect(provider).then(this.start_game.bind(this));
+            } else {
+                firebase.database().ref("/users/" + firebase.auth().currentUser.uid).once("value").then(this.start_game.bind(this));
+            }
+        };
+
+        this.start_game = function () {
+            "use strict";
+            this.scene.start("PlayGame");
+        };
+        this.login();
+    }
+
+
+    firebaseLogout(){
+        firebase.auth().signOut().then(function() {
+            // Redirect to google sign out.
+            window.location.assign('https://accounts.google.com/logout');
+        });
+    }
+
+
+    spinEarth() {
+        if (this.canSpin) {
+            this.canSpin = false;
+            this.tweens.add({
+                targets: [this.logo],
+                angle: 360,
+                duration: 1000,
+                ease: "Cubic.easeOut",
+                callbackScope: this,
+                onComplete: function() {
+                    this.canSpin = true;
+                }
+            });
+        }
+
+        this.countSpin += 1;
+        if (this.countSpin == 3) {
+            this.nyanCat();
+            this.countSpin = 0;
+        }
+    }
+
+
+    nyanCat(){
+        this.cat = this.physics.add.sprite(-10, Math.random() * this.canvasGame.height, "cat", 0).setScale(3);
+        this.cat_2 = this.physics.add.sprite(-50, Math.random() * this.canvasGame.height, "cat", 0).setScale(3);
+        this.cat_3 = this.physics.add.sprite(-20, Math.random() * this.canvasGame.height, "cat", 0).setScale(3);
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('cat', { start: 0, end: 9 }),
+            frameRate: 9,
+            repeat: -1
+        });
+
+        this.cat.setVelocityX(300);
+        this.cat.anims.play('right', true);
+        this.cat_2.setVelocityX(200);
+        this.cat_2.anims.play('right', true);
+        this.cat_3.setVelocityX(400);
+        this.cat_3.anims.play('right', true);
+    }
+}
---
+++ js/end.js
@@ -0,0 +1,295 @@
+class EndScene extends Phaser.Scene {
+    constructor() {
+        super("EndGame");
+    }
+
+
+    preload() {
+        this.canvas1 = document.getElementsByTagName("canvas");
+        this.canvas1[0].setAttribute("id", "canvasGame");
+        this.canvasGame = document.getElementById("canvasGame");
+        this.add.image(0, 0, "bg").setOrigin(0);
+    }
+
+
+    create() {
+        this.leaderboard = this.add.text(this.canvasGame.width / 4, this.canvasGame.height * 0.8,
+            "Leaderboard", { fill: "#FFFFFF", fontSize: "3em", fontFamily: 'abel-regular'});
+        this.leaderboard.setInteractive().setOrigin(0.5, 0);
+        this.leaderboard.on("pointerdown", function(){
+            console.log("Hi")
+        }, this);
+
+        this.restart = this.add.text(this.canvasGame.width * 3 / 4, this.canvasGame.height * 0.8,
+            "restart", { fill: "#FFFFFF", fontSize: "3em", fontFamily: 'abel-regular'});
+        this.restart.setInteractive().setOrigin(0.5, 0);
+        this.restart.on("pointerdown", function(){
+            restartStat();
+            this.scene.start("PlayGame");
+        }, this);
+
+
+        this.setupIcons();
+        this.createEarth();
+        this.updateEarth();
+        getPlayerNum();
+        this.gameOver();
+    }
+
+
+    gameOver(){
+        if (getAverage() > 300){
+            this.createCard(getGoodEnding(), getEndingBack());
+        }
+        else {
+            this.createCard(getBadEnding(), getEndingBack());
+        }
+        this.flipCard();
+    }
+
+
+    createEarth() {
+        this.earthContainer = this.add.container(this.canvasGame.width / 2, this.canvasGame.height / 2);
+        this.earth_water = this.add.image(0, 0, "earth_water");
+        this.earth_dirty_water_1 = this.add.image(0, 0, "earth_dirty_water_1");
+        this.earth_dirty_water_2 = this.add.image(0, 0, "earth_dirty_water_2");
+        this.earth_dirty_water_3 = this.add.image(0, 0, "earth_dirty_water_3");
+
+        this.earth_dirty_water_1.visible = false;
+        this.earth_dirty_water_2.visible = false;
+        this.earth_dirty_water_3.visible = false;
+
+        this.earth_land = this.add.image(0, 0, "earth_land");
+        this.earth_dirty_land_1 = this.add.image(0,0, "earth_dirty_land_1");
+        this.earth_dirty_land_2 = this.add.image(0, 0, "earth_dirty_land_2");
+        this.earth_dirty_land_3 = this.add.image(0, 0, "earth_dirty_land_3");
+
+        this.earth_dirty_land_1.visible = false;
+        this.earth_dirty_land_2.visible = false;
+        this.earth_dirty_land_3.visible = false;
+
+        this.clean_mt = this.add.image(0,0, "clean_mt");
+        this.default_mt = this.add.image(0,0, "default_mt");
+        this.dirty_mt = this.add.image(0,0, "dirty_mt");
+        this.clean_clouds = this.add.image(0,0, "clean_clouds");
+        this.dirty_clouds = this.add.image(0,0, "dirty_clouds");
+
+        this.clean_mt.visible = false;
+        this.dirty_mt.visible = false;
+        this.dirty_clouds.visible = false;
+
+        this.bush_1 = this.add.image(0, 0, "bush_1");
+        this.bush_2 = this.add.image(0, 0, "bush_2");
+
+        this.tree_1 = this.add.image(0, 0, "tree_1");
+        this.tree_2 = this.add.image(0, 0, "tree_2");
+        this.tree_3 = this.add.image(0, 0, "tree_3");
+        this.tree_4 = this.add.image(0, 0, "tree_4");
+
+        this.tree_3.visible = false;
+        this.tree_4.visible = false;
+
+        this.fish_1 = this.add.image(0, 0, "fish_1");
+        this.fish_2 = this.add.image(0, 0, "fish_2");
+
+        this.fish_2.visible = false;
+
+        this.whale_1 = this.add.image(0, 0, "whale_1");
+        this.whale_2 = this.add.image(0, 0, "whale_2");
+
+        this.whale_2.visible = false;
+
+        this.salmon_1 = this.add.image(0, 0, "salmon_1");
+        this.salmon_2 = this.add.image(0, 0, "salmon_2");
+
+        this.salmon_2.visible = false;
+
+        this.tuna_1 = this.add.image(0, 0, "tuna_1");
+        this.tuna_2 = this.add.image(0, 0, "tuna_2");
+
+        this.tuna_2.visible = false;
+
+        this.cow_1 = this.add.image(0, 0, "cow_1");
+        this.cow_2 = this.add.image(0, 0, "cow_2");
+
+        this.cow_2.visible = false;
+
+        this.pig_1 = this.add.image(0, 0, "pig_1");
+        this.pig_2 = this.add.image(0, 0, "pig_2");
+
+        this.pig_2.visible = false;
+
+        this.shrimp_1 = this.add.image(0, 0, "shrimp_1");
+        this.shrimp_2 = this.add.image(0, 0, "shrimp_2");
+
+        this.shrimp_2.visible = false;
+
+        this.factory_1= this.add.image(0, 0, "factory_1");
+        this.factory_2 = this.add.image(0, 0, "factory_2");
+        this.factory_2.visible = false;
+
+        this.house_1 = this.add.image(0, 0, "house_1");
+        this.house_2 = this.add.image(0, 0, "house_2");
+        this.house_2.visible = false;
+
+        this.earthContainer.add([this.earth_water, this.earth_dirty_water_1, this.earth_dirty_water_2,
+            this.earth_dirty_water_3, this.earth_land, this.earth_dirty_land_1, this.earth_dirty_land_2,
+            this.earth_dirty_land_3, this.bush_1, this.bush_2, this.tree_1, this.tree_2, this.tree_3, this.tree_4,
+            this.default_mt, this.clean_mt, this.dirty_mt,
+            this.clean_clouds, this.dirty_clouds,
+            this.fish_1, this.fish_2,
+            this.whale_1, this.whale_2,
+            this.salmon_1, this.salmon_2, this.tuna_1, this.tuna_2,
+            this.cow_1, this.cow_2, this.pig_1, this.pig_2, this.shrimp_1, this.shrimp_2,
+            this.factory_1, this.factory_2, this.house_1, this.house_2]);
+    }
+
+
+    updateEarth() {
+        if (getEnvironment() < 50) {
+            this.earth_dirty_water_3.visible = true;
+            this.earth_dirty_land_3.visible = true;
+            console.log("environment < 50 = bad earth");
+        }
+        if (getEnvironment() > 50) {
+            this.earth_dirty_water_3.visible = false;
+            this.earth_dirty_land_3.visible = false;
+            console.log("environment > 50 = good earth");
+        }
+        if (getResources() > 50) {
+            this.tree_3.visible = true;
+            this.tree_4.visible = true;
+            console.log("resource > 50 = add trees");
+        }
+        if (getResources() < 50) {
+            this.tree_3.visible = false;
+            this.tree_4.visible = false;
+            console.log("resource < 50 = remove trees");
+        }
+        if (getEconomy() > 50) {
+            this.factory_2.visible = true;
+            console.log("economy > 50 = add factories");
+        }
+        if (getEconomy() < 50) {
+            this.factory_2.visible = false;
+            console.log("economy < 50 = remove factories");
+        }
+        if (getSociety() > 50) {
+            this.house_2.visible = true;
+            console.log("society > 50 = add houses");
+        }
+        if (getSociety() < 50) {
+            this.house_2.visible = false;
+            console.log("society < 50 = remove houses");
+        }
+    }
+
+
+    createCard(textFront, textBack) {
+        this.card = this.add.image(0, 0, "card").setInteractive();
+        this.card.setScale(2.75);
+        this.card.alpha = 0.7;
+        // this.currentEvent = getRandomEvent();
+
+        let textStyle = {
+            color:'#000000',
+            align:"center",
+            fontFamily: 'abel-regular',
+            boundsAlignH: "center",
+            fontSize: '60px',
+            wordWrap: {
+                width: this.card.width * 2.25,
+                useAdvancedWrap: false
+            }
+        };
+
+        this.question = this.add.text(0, 0, textFront, textStyle).setOrigin(0.5);
+        this.info = this.add.text(0, 0, textBack, textStyle).setOrigin(0.5);
+        this.info.visible = false;
+        //container for the card
+        this.container = this.add.container(this.canvasGame.width / 2, this.canvasGame.height / 2)
+            .setSize(this.canvasGame.width * 0.5, this.canvasGame.width * 0.5)
+            .setInteractive();
+        this.container.add([this.card, this.question, this.info]);
+    }
+
+
+    flipCard(){
+        this.card.on('pointerup', function(){
+            this.tweens.add({
+                targets: this.card,
+                scaleY: 2.9,
+                scaleX: 0,
+                flipX: true,
+                duration: 200,
+            });
+
+            this.tweens.add({
+                targets: [this.question, this.info],
+                scaleY: 1.2,
+                scaleX: 0,
+                flipX: true,
+                duration: 200,
+            });
+
+
+            this.time.delayedCall(200, function(){
+                if (this.question.visible){
+                    this.info.visible = true;
+                    this.question.visible = false;
+                } else {
+                    this.info.visible = false;
+                    this.question.visible = true;
+                }
+                this.tweens.add({
+                    targets: this.card,
+                    scaleY: 2.75,
+                    scaleX: 2.75,
+                    flipX: true,
+                    duration: 200,
+                });
+                this.tweens.add({
+                    targets: [this.question, this.info],
+                    scaleY: 1,
+                    scaleX: 1,
+                    flipX: true,
+                    duration: 200,
+                });
+            }, this.card, this);
+        }, this);
+    }
+
+
+    setupIcons() {
+        // Under icons
+        this.add.image(this.canvasGame.width / 2 - 330, 150, 'env').setScale(0.4);
+        this.add.image(this.canvasGame.width / 2 - 110, 150, 'soc').setScale(0.4);
+        this.add.image(this.canvasGame.width / 2 + 110, 150, 'eco').setScale(0.4);
+        this.add.image(this.canvasGame.width / 2 + 330, 150, 'res').setScale(0.4);
+
+        // Over icons
+        this.envMask = this.add.image(this.canvasGame.width / 2 - 330, 150, 'env').setScale(0.4);
+        this.envMask.tint = 0x808080;
+        this.socMask = this.add.image(this.canvasGame.width / 2 - 110, 150, 'soc').setScale(0.4);
+        this.socMask.tint = 0x808080;
+        this.ecoMask = this.add.image(this.canvasGame.width / 2 + 110, 150, 'eco').setScale(0.4);
+        this.ecoMask.tint = 0x808080;
+        this.resMask = this.add.image(this.canvasGame.width / 2 + 330, 150, 'res').setScale(0.4);
+        this.resMask.tint = 0x808080;
+
+        this.updateIcons();
+    }
+
+
+    updateIcons() {
+        this.cropIcon(this.envMask, getEnvironment());
+        this.cropIcon(this.socMask, getSociety());
+        this.cropIcon(this.ecoMask, getEconomy());
+        this.cropIcon(this.resMask, getResources());
+    }
+
+
+    cropIcon(icon, percent) {
+        icon.setCrop(0, icon.height - icon.height * (percent / 100), 1000, 1000);
+    }
+}

4af1a4f5b1b53bcd3ed76940a5691a53ea67c976
truongnguyenlinh <ltruong28@my.bcit.ca>
2019-05-22 12:36:20 -0700

    Refactor questions and add correct path for bgm.

--- js/boot.js
+++ js/boot.js
@@ -17,7 +17,7 @@ class BootScene extends Phaser.Scene {
         this.load.image("unmute", "./assets/images/unmute.png");
         this.load.image("mute", "./assets/images/mute.png");
 
-        this.load.audio('bgm','./assets/bgm.mp3');
+        this.load.audio('bgm','./assets/sounds/bgm.mp3');
         this.load.audio('bgm','./assets/sounds/bgm.mp3');
         this.load.audio('sfxButton','./assets/sounds/button.wav');
         this.load.audio('sfxCard','./assets/sounds/card.mp3');
